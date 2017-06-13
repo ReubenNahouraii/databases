2beb89c31fcee9da6e93dd5f92b078fc337ff4fb
Hack Reactor Student <student@hackreactor.com>
2017-06-13 15:30:35 -0700

    implement post method for messages

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -4,8 +4,14 @@ module.exports = {
   messages: {
     get: function (req, res) {}, // a function which handles a get request for all messages
     post: function (req, res) {
-      models.messages.post(req.body);
-      res.status(201).end(JSON.stringify(req.body));
+      models.messages.post(req.body, (err) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        res.status(201).end(JSON.stringify(req.body));
+      });
+      
     } // a function which handles posting a message to the database
   },
 
--- server/models/index.js
+++ server/models/index.js
@@ -1,29 +1,65 @@
 var db = require('../db');
 
 
-let insertIntoMessages = function(userId, messageObject) {
+let insertIntoMessages = function(userId, messageObject, callback) { // callback with userid
   // takes in userId
   let sql = `insert into messages values (null, '${messageObject.message}', '${userId}')`;
   db.con.query(sql, (err, results) => {
     if (err) {
       console.log(err);
+      callback(err);
+      return;
     }
+    callback(null);
     
   });
 };
 
+
+
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function (messageObject) {
-      let userId = `select userId where name = '${messageObject.name}'`;
-      let sql = ``;
+    get: function () {
+      
+      
+    }, // a function which produces all the messages
+    post: function (messageObject, callback) {
+      
+        // select id in user where name = same as mss Obj name 
+      let sql = `select userId from users where name = '${messageObject.name}'`;
       db.con.query(sql, (err, results) => {
+        console.log('RESULTS', results);
         if (err) {
           console.log(err);
+          callback(err);
+          return;
         }
-        
+        let userIdObj = results[0];
+        if (userIdObj !== undefined) {
+          insertIntoMessages(userIdObj.userId, messageObject, callback);
+        } else {
+          // inset users first
+          let sql = `insert into users values (null,'${messageObject.name}')`;
+          db.con.query(sql, (err, results) => {
+            if (err) {
+              console.log(err);
+              callback(err);
+              return;
+            }
+            insertIntoMessages(results.insertId, messageObject, callback);
+          });
+        }
+          
+          
       });
+        // if userid exists from results.id callback of resultsid   then insert values ti that id into the messages table
+        
+          // else need to go and insert that name into the user table then get thae id from results and finally insert into the messafeg table
+          
+           
+      
+      // let sql = `insert into messages values (null, '${messageobject.message}', (select userid from user where name = '${messageobject.name}')`; /// select from user id where name = julia
+
     } // a function which can be used to insert a message into the database
   },
 
